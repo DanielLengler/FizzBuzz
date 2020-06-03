b78ce1a55c946e4aaaa4171b67ad477dfdb429c6
Asis <47801367+Asis2019@users.noreply.github.com>
2020-06-03 13:31:01 -0400

    Small code improvements

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,13 +1,19 @@
+multiples_list = [[3, "Fizz"], [8, "Buzz"]]
+
+
+def game():
+    for i in range(1, int(play_until) + 1):
+        output = ""
+
+        for multiple in multiples_list:
+            if i % multiple[0] == 0:
+                output += multiple[1]
+
+        if output == "":
+            output += str(i)
+
+        print(output)
+
+
 play_until = input("How many rounds do you want to play?")
-
-for i in range(1, int(play_until) + 1):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-        continue
-
-    if i % 3 == 0:
-        print("Fizz")
-    elif i % 5 == 0:
-        print("Buzz")
-    else:
-        print(i)
+game()
